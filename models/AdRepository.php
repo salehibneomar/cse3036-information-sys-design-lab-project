2171708eddfa2bb575fc849a9b7100f9bcf934ae
Saleh <salehibneomar@gmail.com>
2020-06-27 16:13:58 +0600

    fix

--- models/AdRepository.php
+++ models/AdRepository.php
@@ -4,7 +4,7 @@
 
         public static function viewAdById($adId);
         public static function getAllAds();
-        public static function getAddListByUserId($id);
+        public static function getAdListByUserId($id);
         public static function createAd($ad, $userId);
         public static function adReport($report);
         public static function deleteAd($userId, $adId);
